3b801ce0ed935a07b2a3a12210b03c022a172e3b
Chris Woodruff <cwoodruff@live.com>
2014-10-25 14:47:35 -0400

    no message

---
+++ ChinookWebAPIOData.Tests/Controllers/AlbumsControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChinookWebAPIOData.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChinookWebAPIOData.Controllers.Tests
+{
+    [TestClass()]
+    public class AlbumsControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            AlbumsController album = new AlbumsController();
+            Assert.Fail();
+        }
+    }
+}
